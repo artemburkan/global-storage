cc4d1ff18fbbd5cacbc6719283e99661aa17c0bf
Artem Burkan <job.artem.burkan@gmail.com>
2024-07-24 00:43:52 +0300

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,23 @@
-import path from 'path'
-import { generateScopedName } from './config-utils/generateScopedName'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import postcssNesting from 'postcss-nesting';
+import path from "path"
+import { generateScopedName } from "./config-utils/generateScopedName"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import postcssNesting from "postcss-nesting"
 
 export default defineConfig({
+  base: "/global-storage/",
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "./src/")
-    }
+      "~": path.resolve(__dirname, "./src/"),
+    },
   },
 
   css: {
     modules: {
-      generateScopedName
+      generateScopedName,
     },
     postcss: {
-      plugins: [
-        postcssNesting
-      ],
+      plugins: [postcssNesting],
     },
   },
   plugins: [react()],
